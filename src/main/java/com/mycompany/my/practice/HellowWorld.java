6e38406c359631fc95ba0501f169be224778c7e4
Batna Chandra Sekhar <sekharu506@gmail.com>
2020-05-26 05:29:45 -0700

    Adding Hellow world

---
+++ src/main/java/com/mycompany/my/practice/HellowWorld.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.my.practice;
+
+/**
+ *
+ * @author Batna Chandra Sekhar
+ */
+public class HellowWorld {
+    public static void main(String[] args) {
+        System.out.println("Hellow world");
+    }
+}
