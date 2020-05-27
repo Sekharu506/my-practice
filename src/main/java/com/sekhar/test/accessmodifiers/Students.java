225e117c74b078aa20a1ae0d4c4dec3f371221de
Batna Chandra Sekhar <sekharu506@gmail.com>
2020-05-27 05:25:03 -0700

    Access modifiers

--- src/main/java/com/sekhar/test/accessmodifiers/Students.java
+++ src/main/java/com/sekhar/test/accessmodifiers/Students.java
@@ -3,9 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.sekhar.StudentDetails;
+package com.sekhar.test.accessmodifiers;
 
-/**
+/**s
  *
  * @author Batna Chandra Sekhar
  */
--- src/main/java/com/sekhar/test/accessmodifiers/TestStudent.java
+++ src/main/java/com/sekhar/test/accessmodifiers/TestStudent.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.sekhar.StudentTest;
-import com.sekhar.StudentDetails.StudentData;
+package com.sekhar.test.accessmodifiers;
+
 /**
  *
  * @author Batna Chandra Sekhar
  */
-public class Test {
+public class TestStudent {
 
     public static void main(String a[]) {
         showAll();
