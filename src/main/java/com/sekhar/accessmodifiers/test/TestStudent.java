21066abfc9c122a88ad25d4dbdbefa6b95757c0d
Krishna Rao Chintu <krishna.chintu@wavemaker.com>
2020-05-28 14:54:47 +0530

    refactor for test file

--- src/main/java/com/sekhar/accessmodifiers/test/TestStudent.java
+++ src/main/java/com/sekhar/accessmodifiers/test/TestStudent.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.sekhar.accessmodifierstest;
+package com.sekhar.accessmodifiers.test;
 
 import com.sekhar.accessmodifiers.Student;
 
